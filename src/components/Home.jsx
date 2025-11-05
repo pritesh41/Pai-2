4ec9b51decb013e452ddce4f7e7bcbbd0db17d01
Pritesh <149871904+pritesh41@users.noreply.github.com>
2025-11-05 23:16:38 +0530

    Refactor Home component to HomePage with updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,69 @@
-import React, {useState,useEffect}from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { fetchProducts, setSearch, setCategory, setPriceRange, clearFilters, loadMore, applyFilters} from './productSlice'
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  fetchProducts,
+  setSearch,
+  setCategory,
+  setPriceRange,
+  clearFilters,
+  loadMore,
+  applyFilters,
+} from './productSlice';
 
-function Home() {
-    const dispatch= useDispatch();
-    const { fetchProducts, setSearch, setCategory, setPriceRange, clearFilters, loadMore, applyFilters} =useSelector(state => state.products);
+function HomePage() {
+  const dispatch = useDispatch();
+  const {
+    filteredProducts,
+    status,
+    search,
+    category,
+    priceRange,
+    visibleCount,
+    allProducts
+  } = useSelector(state => state.products);
 
-    const [searchInput, setSearchInput] = useState('');
-    useEffect(()=> {
-        const handler = setTimeout(()=> {
-            dispatch(setSearch(searchInput));
-            dispatch(applyFilters);
-        }, 500);
-        return () => clearTimeout(handler);
-    }, [searchInput, dispatch]);
+  const [searchInput, setSearchInput] = useState('');
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      dispatch(setSearch(searchInput));
+      dispatch(applyFilters());
+    }, 500);
+    return () => clearTimeout(handler);
+  }, [searchInput, dispatch]);
 
-    useEffect(()=>{
-        dispatch(applyFilters());
-    }, [search, category. priceRange, allProducts, dispatch]);
-    useEffect(()=> {
-        dispatch(fetchProducts());
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(applyFilters());
+  }, [search, category, priceRange, allProducts, dispatch]);
+
+  useEffect(() => { dispatch(fetchProducts()); }, [dispatch]);
 
   return (
     <div>
-      
+      <input
+        placeholder="Search..."
+        value={searchInput}
+        onChange={e => setSearchInput(e.target.value)}
+      />
+      <button onClick={() => dispatch(clearFilters())}>Clear Filters</button>
+
+      {status === 'loading' ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <ul>
+            {filteredProducts.slice(0, visibleCount).map(product => (
+              <li key={product.id}>
+                {product.title} - ${product.price}
+              </li>
+            ))}
+          </ul>
+          {visibleCount < filteredProducts.length && (
+            <button onClick={() => dispatch(loadMore())}>Load More</button>
+          )}
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default Home
+export default HomePage;
